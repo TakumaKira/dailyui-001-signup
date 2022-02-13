9879b12a3d5e8bdfa2a2105e09b054223f5e5ce3
Takuma Kira <takuma@kirakiraworx.com>
2022-02-13 15:05:17 +0900

    [dev] Change the style

--- signup-form.js
+++ signup-form.js
@@ -1,18 +1,28 @@
 import 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
 
 const template = document.createElement('template')
-template.innerHTML = `
+const elements = `
   <form id="form">
-    <h1 id="title"></h1>
-    <input id="email" name="email" type="text" />
-    <span id="email-error" class="error"></span>
-    <input id="password" name="password" type="password" />
-    <span id="password-error" class="error"></span>
-    <button id="button" type="submit"></button>
+  <h1 id="title"></h1>
+
+  <div id="email-input-wrapper" class="input-wrapper">
+    <input id="email" name="email" type="text" placeholder=" " />
+    <label id="email-placeholder" for="email"></label>
+  </div>
+  <span id="email-error" class="error"></span>
+
+  <div id="password-input-wrapper" class="input-wrapper">
+    <input id="password" name="password" type="password" placeholder=" " />
+    <label id="password-placeholder" for="password"></label>
+  </div>
+  <span id="password-error" class="error"></span>
+
+  <button id="button" type="submit"></button>
   </form>
 
   <h1 id="complete" hidden></h1>
-
+`
+const styles = `
   <style>
     :host {
       box-sizing: border-box;
@@ -24,7 +34,7 @@ template.innerHTML = `
       align-items: center;
     }
     form {
-      width: 380px;
+      width: 400px;
       display: flex;
       flex-direction: column;
       align-items: center;
@@ -34,22 +44,63 @@ template.innerHTML = `
       margin-block-start: 0.3em;
       margin-block-end: 0.3em;
     }
+    .input-wrapper {
+      position: relative;
+      width: 100%;
+      margin-top: 15px;
+      display: flex;
+      align-items: center;
+    }
+    .input-wrapper::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      background-color: #FFFFFF;
+      height: 1px;
+      transition: height 0.3s;
+    }
+    .input-wrapper.focus::after, .input-wrapper.has-value::after {
+      height: 2px;
+    }
     input {
-      margin: 4px auto;
+      display: block;
       width: 95%;
-      border-radius: 2px;
       border: none;
       outline: none;
+      background-color: transparent;
     }
     input[type="text"], input[type="password"] {
       font-family: 'Poppins', sans-serif;
       font-size: 16px;
       padding: 5px 10px;
+      color: #FFFFFF;
+    }
+    input:-webkit-autofill,
+    input:-webkit-autofill:hover,
+    input:-webkit-autofill:focus,
+    input:-webkit-autofill:active{
+      -webkit-text-fill-color: #FFFFFF;
+      -webkit-box-shadow: 0 0 0px 1000px rgba(0,0,0,0) inset;
+      transition: background-color 5000s ease-in-out 0s;
+    }
+    label {
+      position: absolute;
+      margin-left: 10px;
+      transform-origin: center left;
+      transition: all 0.3s;
+      color: #FFFFFF;
+      opacity: 0.5;
+    }
+    input:focus + label, input:not(:placeholder-shown) + label {
+      transform: translateY(-100%) scale(80%);
     }
     .error {
       font-size: 15px;
       align-self: flex-start;
-      margin: 0 5px;
+      margin-top: 5px;
+      margin-bottom: 15px;
+      margin-left: 5px;
     }
     button {
       border-radius: 2px;
@@ -61,6 +112,10 @@ template.innerHTML = `
       padding: 5px 20px;
     }
   </style>
+`
+template.innerHTML = `
+  ${elements}
+  ${styles}
 `
 
 export class SignupForm extends HTMLElement {
@@ -116,9 +171,13 @@ export class SignupForm extends HTMLElement {
     this.rootElem = this.root.host
     this.titleElem = this.root.querySelector('#title')
     this.formElem = this.root.querySelector('#form')
+    this.emailInputWrapperElem = this.root.querySelector('#email-input-wrapper')
     this.emailElem = this.root.querySelector('#email')
+    this.emailPlaceholderElem = this.root.querySelector('#email-placeholder')
     this.emailErrorElem = this.root.querySelector('#email-error')
+    this.passwordInputWrapperElem = this.root.querySelector('#password-input-wrapper')
     this.passwordElem = this.root.querySelector('#password')
+    this.passwordPlaceholderElem = this.root.querySelector('#password-placeholder')
     this.passwordErrorElem = this.root.querySelector('#password-error')
     this.buttonElem = this.root.querySelector('#button')
     this.completeElem = this.root.querySelector('#complete')
@@ -166,13 +225,65 @@ export class SignupForm extends HTMLElement {
       this.completeElem.removeAttribute('hidden')
     }
 
-    // Hide error message on next input
+    // input change events
     this.emailElem.oninput = e => {
+      // Add has-value class when having a value
+      const hasValue = this.emailElem.value !== ''
+      const original = this.emailInputWrapperElem.getAttribute('class')
+      if (hasValue) {
+        this.emailInputWrapperElem.setAttribute('class', this.addClass(original, 'has-value'))
+      } else {
+        this.emailInputWrapperElem.setAttribute('class', this.removeClass(original, 'has-value'))
+      }
+
+      // Hide error message on next input
       this.emailErrorElem.textContent = ''
     }
     this.passwordElem.oninput = e => {
+      // Add has-value class when having a value
+      const hasValue = this.passwordElem.value !== ''
+      const original = this.passwordInputWrapperElem.getAttribute('class')
+      if (hasValue) {
+        this.passwordInputWrapperElem.setAttribute('class', this.addClass(original, 'has-value'))
+      } else {
+        this.passwordInputWrapperElem.setAttribute('class', this.removeClass(original, 'has-value'))
+      }
+
+      // Hide error message on next input
       this.passwordErrorElem.textContent = ''
     }
+
+    // Add focus class when focused
+    this.emailElem.onfocus = e => {
+      const original = this.emailInputWrapperElem.getAttribute('class')
+      this.emailInputWrapperElem.setAttribute('class', this.addClass(original, 'focus'))
+    }
+    this.emailElem.onblur = e => {
+      const original = this.emailInputWrapperElem.getAttribute('class')
+      this.emailInputWrapperElem.setAttribute('class', this.removeClass(original, 'focus'))
+    }
+    this.passwordElem.onfocus = e => {
+      const original = this.passwordInputWrapperElem.getAttribute('class')
+      this.passwordInputWrapperElem.setAttribute('class', this.addClass(original, 'focus'))
+    }
+    this.passwordElem.onblur = e => {
+      const original = this.passwordInputWrapperElem.getAttribute('class')
+      this.passwordInputWrapperElem.setAttribute('class', this.removeClass(original, 'focus'))
+    }
+  }
+
+  addClass(original, newClass) {
+    const classes = original.split(' ')
+    if (classes.includes(newClass))
+      return original
+    classes.push(newClass)
+    return classes.join(' ')
+  }
+
+  removeClass(original, oldClass) {
+    const classes = original.split(' ')
+    const filtered = classes.filter(c => c !== oldClass)
+    return filtered.join(' ')
   }
 
   initializeParams() {
@@ -252,8 +363,8 @@ export class SignupForm extends HTMLElement {
   render() {
     this.titleElem.textContent = this.title
     this.buttonElem.textContent = this.submitButtonTitle
-    this.emailElem.placeholder = this.emailPlaceholder
-    this.passwordElem.placeholder = this.passwordPlaceholder
+    this.emailPlaceholderElem.textContent = this.emailPlaceholder
+    this.passwordPlaceholderElem.textContent = this.passwordPlaceholder
     this.completeElem.textContent = this.completeMessage
     this.rootElem.setAttribute('style', `background-color: ${this.backgroundColor}`)
     this.titleElem.setAttribute('style', `color: ${this.titleColor}`)
